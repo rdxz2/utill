a0a14b0e118812b503430d6c1108935c336107fe
rdxz2 <richard.dharmawan@gmail.com>
2024-12-22 12:12:01 +0700

    [STRING] add replace non numeric

--- src/utill/my_string.py
+++ src/utill/my_string.py
@@ -1,5 +1,9 @@
 import random
+import re
 import string
 
 
 def generate_random_string(length: int = 4): return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+
+def replace_nonnumeric(string: str, replace: str) -> str: return re.sub('[^0-9a-zA-Z]+', replace, string)
