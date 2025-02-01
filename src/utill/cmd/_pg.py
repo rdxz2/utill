f6d19c0bf6d410fba5de26a74c52af086314fe37
rdxz2 <richard.dharmawan@gmail.com>
2025-02-01 12:02:06 +0700

    [PG] add cmd for pg-to-pg and upload-csv

---
+++ src/utill/cmd/_pg.py
@@ -0,0 +1,15 @@
+def _pg_to_pg(src_profile: str, src_table: str, dst_profile: str, dst_table: str, columns: str):
+    from ..my_pg import PG
+
+    columns = ','.join([f"{x}" for x in columns.split(',')]) if columns != '*' else None
+    pg_src = PG(src_profile)
+    pg_dst = PG(dst_profile)
+
+    pg_src.pg_to_pg(pg_dst, src_table, dst_table, columns)
+
+
+def _upload_csv(profile: str, src_csv: str, dst_table: str):
+    from ..my_pg import PG
+
+    pg = PG(profile)
+    pg.upload_csv(src_csv, dst_table)
