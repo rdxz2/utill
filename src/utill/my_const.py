5d245b5c009fe77ddcf5616dd312867e350e2d69
rdxz2 <richard.dharmawan@gmail.com>
2025-02-06 08:20:30 +0700

    [CONST] add httpmethod

--- src/utill/my_const.py
+++ src/utill/my_const.py
@@ -1,5 +1,18 @@
+from enum import Enum
+
+
 class ByteSize:
     KB = 1_024
     MB = 1_048_576
     GB = 1_073_741_824
     TB = 1_099_511_627_776
+
+
+class HttpMethod(Enum):
+    GET = 1
+    POST = 2
+    PUT = 3
+    DELETE = 4
+
+    def __str__(self):
+        return self.name
