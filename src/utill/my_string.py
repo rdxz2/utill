4b7a427cb78338e38378d315159ffef1473443ee
rdxz2 <richard.dharmawan@gmail.com>
2025-02-06 08:20:02 +0700

    [STRING] add masking

--- src/utill/my_string.py
+++ src/utill/my_string.py
@@ -1,3 +1,4 @@
+import hashlib
 import random
 import re
 import string
@@ -7,3 +8,13 @@ def generate_random_string(length: int = 4, alphanum: bool = False): return ''.j
 
 
 def replace_nonnumeric(string: str, replace: str) -> str: return re.sub('[^0-9a-zA-Z]+', replace, string)
+
+
+def mask(string: str, mask_length_min: int = 5, mask_length_max: int = 50, display_length: int = 5) -> str:
+    if not string:
+        mask_length = mask_length_min
+    else:
+        hash_value = int(hashlib.sha256(string.encode()).hexdigest(), 16)
+        mask_length = mask_length_min + (hash_value % (mask_length_max - mask_length_min + 1))
+
+    return ('*' * mask_length) + (string[(-display_length if len(string) > display_length else -1):])
