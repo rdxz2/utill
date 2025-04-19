fd4e4677cc3c6777bc96a7c10dd164974975b03c
rdxz2-julo <richard.dharmawan@julofinance.com>
2025-04-19 09:05:56 +0700

    [STRING] fix alphanum logic

--- src/utill/my_string.py
+++ src/utill/my_string.py
@@ -4,7 +4,7 @@ import re
 import string
 
 
-def generate_random_string(length: int = 4, alphanum: bool = False): return ''.join(random.choice(string.ascii_letters + string.digits + (r'!@#$%^&*()-=_+[]{};\':",./<>?' if alphanum else '')) for _ in range(length))
+def generate_random_string(length: int = 4, alphanum: bool = False): return ''.join(random.choice(string.ascii_letters + string.digits + (r'!@#$%^&*()-=_+[]{};\':",./<>?' if not alphanum else '')) for _ in range(length))
 
 
 def replace_nonnumeric(string: str, replace: str) -> str: return re.sub('[^0-9a-zA-Z]+', replace, string)
